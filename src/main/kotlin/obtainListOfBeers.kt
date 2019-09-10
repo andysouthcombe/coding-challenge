43c0a5a42fbb4966338cbd322814d087efa80c8d
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2019-09-10 14:08:24 +0100

    sets guest flag for guest beers and outputs guest beers

--- src/main/kotlin/obtainListOfBeers.kt
+++ src/main/kotlin/obtainListOfBeers.kt
@@ -19,5 +19,5 @@ fun getPubsInArea(inputJson: String): List<Pub> {
 
 fun obtainListOfBeers(inputJson: String): List<Beer> {
     val pubsInArea = getPubsInArea(inputJson)
-    return pubsInArea.flatMap{pub -> pub.regularBeers.map {name -> Beer(name,pub.name,pub.pubService,true)}}
+    return pubsInArea.flatMap{pub -> pub.regularBeers.map {name -> Beer(name,pub.name,pub.pubService,true)}} + pubsInArea.flatMap{pub -> pub.guestBeers.map {name -> Beer(name,pub.name,pub.pubService,false)}}
 }
