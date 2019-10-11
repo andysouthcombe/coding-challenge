96726e3eb8fce98a71c7dd6fd2bfaa1f32b26960
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2019-10-11 11:37:38 +0100

    generic fold works on integers

--- coding-challenge-14/src/test/kotlin/myFoldTests.kt
+++ coding-challenge-14/src/test/kotlin/myFoldTests.kt
@@ -15,10 +15,16 @@ class MyFoldTests (){
     }
 }
 
-class myGenericFoldTests() {
+class MyGenericFoldTests() {
     @Test
     fun `should work on a list with strings` () {
         val stringList = listOf<String>("a","b","c")
         assertThat(myGenericFold(stringList,"") {acc, element -> acc + element.toString()}).isEqualTo("abc")
+    }
+
+    @Test
+    fun `should work on a list with integers` () {
+        val  intList = listOf<Int>(4,5,6)
+        assertThat(myGenericFold(intList,"") {acc, element -> acc + element.toString()}).isEqualTo("456")
     }
 }
