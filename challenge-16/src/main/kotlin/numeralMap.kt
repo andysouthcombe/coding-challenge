83275300fd163f1ff09c6c1235900fb07cc424d2
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2019-11-15 16:49:47 +0000

    change map to only handle individual characters.  Converts X to 10

--- challenge-16/src/main/kotlin/numeralMap.kt
+++ challenge-16/src/main/kotlin/numeralMap.kt
@@ -1,7 +1,5 @@
 val numeralMap = mapOf(
     "I" to 1,
-    "II" to 2,
-    "III" to 3,
-    "IV" to 4,
-    "V" to 5
+    "V" to 5,
+    "X" to 10
 )
--- challenge-16/src/test/kotlin/romanNumeralToIntegerTests.kt
+++ challenge-16/src/test/kotlin/romanNumeralToIntegerTests.kt
@@ -8,19 +8,11 @@ class RomanNumeralToIntegerTests {
         assertThat(romanNumeralToInteger("I")).isEqualTo(1)
     }
     @Test
-    fun `should convert II to 2` () {
-        assertThat(romanNumeralToInteger("II")).isEqualTo(2)
-    }
-    @Test
-    fun `should convert III to 3` () {
-        assertThat(romanNumeralToInteger("III")).isEqualTo(3)
-    }
-    @Test
-    fun `should convert IV to 4` () {
-        assertThat(romanNumeralToInteger("IV")).isEqualTo(4)
-    }
-    @Test
     fun `should convert V to 5` () {
         assertThat(romanNumeralToInteger("V")).isEqualTo(5)
+    }
+    @Test
+    fun `should convert X to 10` () {
+        assertThat(romanNumeralToInteger("X")).isEqualTo(10)
     }
 }
