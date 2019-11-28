f2bfae01dd04449e589d0a37ff01556c671a6a5f
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2019-11-28 17:52:50 +0000

    resetting roman numeral solution after merge conflict

--- challenge-16/src/main/kotlin/readRomanNumeral.kt
+++ challenge-16/src/main/kotlin/readRomanNumeral.kt
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-fun readRomanNumeral(inputNumeral: String): Int {
-    val numerals = inputNumeral.toCharArray()
-    val numeralsAsIntegers = numerals.map { romanNumeralToInteger(it.toString()) }
-    var runningTotal = 0
-
-    for (n in numeralsAsIntegers.indices) {
-        val comparator = minOf(n+1,numeralsAsIntegers.size-1)
-        if (numeralsAsIntegers[comparator] > numeralsAsIntegers[n])
-            runningTotal -= numeralsAsIntegers[n]
-        else
-            runningTotal +=numeralsAsIntegers[n]
-    }
-    return runningTotal
-=======
 fun readRomanNumeral(inputNumeral: String): Int {
     val numerals = inputNumeral.toCharArray()
     val numeralsAsIntegers = numerals.map { romanNumeralToInteger(it.toString()) }
@@ -25,5 +10,4 @@ fun readRomanNumeral(inputNumeral: String): Int {
             runningTotal +=numeralsAsIntegers[n]
     }
     return runningTotal
->>>>>>> 09de515767c6b01f1b49a1ab6debebb29a25f55c
 }
