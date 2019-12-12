301b6c79251c63f26b8ddce80f3f807a4e5c2e8d
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2019-12-12 12:35:59 +0000

    extracted function for common logic

--- coding-challenge-18/linked_list_functions.py
+++ coding-challenge-18/linked_list_functions.py
@@ -4,37 +4,25 @@ class Node:
         self.next_node = next_node
 
 
-def print_linked_list(first_node):
+def iterate_through_list(first_node, reverse=False):
     current_node = first_node
     output_string = current_node.item
-
     while current_node.next_node is not None:
         current_node = current_node.next_node
-        output_string += " %s" % current_node.item
-
-    output_string += " None"
+        if reverse:
+            output_string = current_node.item + " " + output_string
+        else:
+            output_string += " %s" % current_node.item
     return output_string
+
+
+def print_linked_list(first_node):
+    return iterate_through_list(first_node) + " None"
 
 
 def append_string_to_list(first_node, text):
-    current_node = first_node
-    output_string = current_node.item
-
-    while current_node.next_node is not None:
-        current_node = current_node.next_node
-        output_string += " %s" % current_node.item
-
-    output_string += " %s None" % text
-    return output_string
+    return iterate_through_list(first_node) + " %s None" % text
 
 
-def reverse_linked_list(first_node):
-    current_node = first_node
-    output_string = current_node.item
-
-    while current_node.next_node is not None:
-        current_node = current_node.next_node
-        output_string = current_node.item + " " + output_string
-
-    output_string += " None"
-    return output_string
+def print_linked_list_reverse(first_node):
+    return iterate_through_list(first_node, True) + " None"
