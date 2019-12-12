9f5128ca4ced097932ccf8518a227da81e7a953a
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2019-12-12 12:14:04 +0000

    Appends text string to nodes

--- coding-challenge-18/linked_list_functions.py
+++ coding-challenge-18/linked_list_functions.py
@@ -14,3 +14,14 @@ def print_linked_list(first_node):
 
     output_string += " None"
     return output_string
+
+def append_string_to_list(first_node, text):
+    current_node = first_node
+    output_string = current_node.item
+
+    while current_node.next_node is not None:
+        current_node = current_node.next_node
+        output_string += " %s" % current_node.item
+
+    output_string += " %s None" % text
+    return output_string
