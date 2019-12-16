493bfa6807d8f9dd7003a680ccf1dc55a85b568b
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2019-12-16 10:03:49 +0000

    converts string list to int list

--- coding-challenge-18-v2/linked_list.py
+++ coding-challenge-18-v2/linked_list.py
@@ -3,6 +3,12 @@ class Node:
         self.item = item
         self.next_node = next_node
 
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            return NotImplemented
+
+        return self.item == other.item and self.next_node == other.next_node
+
     def print_label(self):
         if self.next_node is not None:
             return self.item + " "
@@ -19,11 +25,23 @@ def get_description(first_node):
     return output_string
 
 
-def add_to_list(first_node,item_to_add):
+def add_to_list(first_node, item_to_add):
     last_node = first_node
     while last_node.next_node is not None:
-        last_node = last_node.next_node
-
-    new_node = Node(item_to_add,None)
+;
+    new_node = Node(item_to_add, None)
     last_node.next_node = new_node
 
+
+def convert_string_list_to_int_list(input_node):
+    if input_node.next_node is not None:
+        return Node(safe_cast_str_to_int(input_node.item), convert_string_list_to_int_list(input_node.next_node))
+    else:
+        return Node(safe_cast_str_to_int(input_node.item))
+
+
+def safe_cast_str_to_int(value):
+    try:
+        return int(value)
+    except:
+        return int(0)
--- coding-challenge-18-v2/linked_list_tests.py
+++ coding-challenge-18-v2/linked_list_tests.py
@@ -17,8 +17,31 @@ class NodeUnitTests(unittest.TestCase):
         first_node = Node("Hello", second_node)
         self.assertEqual(get_description(first_node), "Hello World None")
 
-    def test_add_node_to_list(self):
+    def test_add_node_to_two_node_list(self):
         second_node = Node("World")
         first_node = Node("Hello", second_node)
         add_to_list(first_node, "!")
-        self.assertEqual(get_description(first_node),"Hello World ! None")
+        self.assertEqual(get_description(first_node), "Hello World ! None")
+
+    def test_add_node_to_one_node_list(self):
+        first_node = Node("Hello")
+        add_to_list(first_node, "!")
+        self.assertEqual(get_description(first_node), "Hello ! None")
+
+    def test_convert_single_node_list_to_integer_list(self):
+        first_node = Node("1")
+        int_first_node = convert_string_list_to_int_list(first_node)
+        self.assertEqual(int_first_node.item, int(1))
+
+    def test_convert_single_invalid_node_to_integer_list(self):
+        first_node = Node("abc")
+        int_first_node = convert_string_list_to_int_list(first_node)
+        self.assertEqual(int_first_node.item, int(0))
+
+    def test_convert_stirng_list_to_int_list(self):
+        third_node = Node("3")
+        second_node = Node("abc", third_node)
+        first_node = Node("27", second_node)
+        int_node_list = convert_string_list_to_int_list(first_node)
+        expected_int_list = Node(27, Node(0, Node(3)))
+        self.assertEqual(int_node_list, expected_int_list)
