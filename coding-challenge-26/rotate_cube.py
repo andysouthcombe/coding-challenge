17af4c7d43cc8ab691642e19bfa0c43f42b83803
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2020-04-28 08:34:56 +0100

    validate cube fails if any face has less than 9 blocks

--- coding-challenge-26/rotate_cube.py
+++ coding-challenge-26/rotate_cube.py
@@ -1,3 +1,7 @@
 def validate_cube(cube_to_validate):
     if len(cube_to_validate) != 6:
         raise ValueError("incorrect number of sides")
+
+    for face in cube_to_validate:
+        if len(face) < 9:
+            raise ValueError("incorrect number of blocks")
--- coding-challenge-26/rotate_cube_tests.py
+++ coding-challenge-26/rotate_cube_tests.py
@@ -5,3 +5,6 @@ from rotate_cube import validate_cube
 class ValidateCubeTests(unittest.TestCase):
     def test_validate_cube_throws_exception_if_not_enough_sides(self):
         self.assertRaises(ValueError, validate_cube, ["GGGGGGGGG","YYYYYYYYY","OOOOOOOOO","RRRRRRRR","WWWWWWWWWWW"])
+
+    def test_validate_cube_throws_exception_if_any_face_has_less_than_nine_blocks(self):
+        self.assertRaises(ValueError, validate_cube, ["GGGGGGGGG","YYYYYYYYY","OOOOOOOOO","RRRRRRR","WWWWWWWWWWW","BBBBBBBBB"],)
