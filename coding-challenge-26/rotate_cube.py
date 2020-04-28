7885bc1933b77ee56a6944f7f403ce3fd1ea1f7a
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2020-04-28 08:58:26 +0100

    validate cube fails if any colour has more or less than 9 occurrences

--- coding-challenge-26/rotate_cube.py
+++ coding-challenge-26/rotate_cube.py
@@ -5,3 +5,8 @@ def validate_cube(cube_to_validate):
     for face in cube_to_validate:
         if len(face) != 9:
             raise ValueError("incorrect number of blocks")
+
+    count_per_colour = [[x,cube_to_validate.count(x)] for x in set(cube_to_validate)]
+
+    if any(colour_count != 9 for colour_count in count_per_colour):
+        raise ValueError("incorrect number of blocks for colour")
--- coding-challenge-26/rotate_cube_tests.py
+++ coding-challenge-26/rotate_cube_tests.py
@@ -1,14 +1,20 @@
 import unittest
 from rotate_cube import validate_cube
 
+correct_cube_input = ["GGGGGGGGG", "YYYYYYYYY", "OOOOOOOOO", "RRRRRRRRR", "WWWWWWWWW", "BBBBBBBBB"]
 
 class ValidateCubeTests(unittest.TestCase):
     def test_validate_cube_throws_exception_if_not_enough_sides(self):
-        self.assertRaises(ValueError, validate_cube, ["GGGGGGGGG","YYYYYYYYY","OOOOOOOOO","RRRRRRRR","WWWWWWWWWWW"])
+        self.assertRaises(ValueError, validate_cube, ["GGGGGGGGG", "YYYYYYYYY", "OOOOOOOOO", "RRRRRRRR", "WWWWWWWWWWW"])
 
     def test_validate_cube_throws_exception_if_any_face_has_less_than_nine_blocks(self):
-        self.assertRaises(ValueError, validate_cube, ["GGGGGGGGG","YYYYYYYYY","OOOOOOOOO","RRRRRRR","WWWWWWWWWWW","BBBBBBBBB"])
+        self.assertRaises(ValueError, validate_cube,
+                          ["GGGGGGGGG", "YYYYYYYYY", "OOOOOOOOO", "RRRRRRR", "WWWWWWWWWWW", "BBBBBBBBB"])
 
     def test_validate_cube_throws_exception_if_any_face_has_more_than_nine_blocks(self):
-        self.assertRaises(ValueError, validate_cube, ["GGGGGGGGG","YYYYYYYYY","OOOOOOOOOO","RRRRRRRRR","WWWWWWWWWWW","BBBBBBBBB"])
+        self.assertRaises(ValueError, validate_cube,
+                          ["GGGGGGGGG", "YYYYYYYYY", "OOOOOOOOO", "RRRRRRRRR", "WWWWWWWWWWW", "BBBBBBBBB"])
 
+    def test_validate_cube_throws_exception_if_any_colour_does_not_have_nine_blocks(self):
+        self.assertRaises(ValueError, validate_cube,
+                          ["GGGGGGGGG", "YYYYYYYYY", "OOOOOOOOO", "RRRRRRRRR", "WWWWWWWWW", "BBBBBBBBB"])
